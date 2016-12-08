7e82dd40cea86fc0ead47b804953b581e6d88c58
Johan Svensson <johan@atomicplayboy.net>
2016-12-08 21:10:29 +0100

    First schema test

--- JsonTesting/Program.cs
+++ JsonTesting/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JsonTesting.Models;
+using NJsonSchema;
 
 namespace JsonTesting
 {
@@ -10,6 +12,10 @@ namespace JsonTesting
     {
         static void Main(string[] args)
         {
+            var schema = JsonSchema4.FromType<Order>();
+            var schemaData = schema.ToJson();
+
+            Console.WriteLine(schemaData);
         }
     }
 }
